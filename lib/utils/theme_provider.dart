e43ecc81f2f7626d8b034c390a1c9527d48e9205
Adem ERCAN <ademerc.ss@gmail.com>
2025-03-15 06:41:00 +0300

    15.03.2025 - 06:40

--- lib/utils/theme_provider.dart
+++ lib/utils/theme_provider.dart
@@ -10,9 +10,41 @@ import 'package:sosyal_ag/utils/constants.dart';
 class ThemeProvider with ChangeNotifier {
 // VARIABLES
   final ThemeData _themeDataLight = ThemeData(
-    primaryColor: Colors.white60,
-    primarySwatch: Colors.blue,
+    primaryColor: Colors.white,
+    scaffoldBackgroundColor: Colors.white,
     brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      primary: Constants.themeColor6,
+      secondary: Colors.grey[100]!,
+      surface: Colors.grey[200]!,
+      onPrimary: Colors.black87,
+      onSecondary: Colors.black54,
+      onSurface: Colors.black,
+      tertiary: Colors.tealAccent[700]!,
+      onError: Colors.redAccent,
+      error: Colors.redAccent,
+      brightness: Brightness.light,
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.grey[100],
+      elevation: 0,
+      iconTheme: const IconThemeData(color: Colors.black87),
+    ),
+    cardTheme: CardTheme(
+      color: Colors.grey[100],
+      elevation: 4,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Constants.themeColor4,
+        foregroundColor: Constants.themeColor1,
+      ),
+    ),
+    textTheme:  TextTheme(
+      titleLarge: GoogleFonts.aBeeZee(color: Constants.themeColor1, fontWeight: FontWeight.bold),
+      bodyLarge: GoogleFonts.aBeeZee(color: Constants.themeColor2),
+      bodyMedium: GoogleFonts.aBeeZee(color: Constants.themeColor2),
+    ),
   );
 
   final ThemeData _themeDataDark = ThemeData(
@@ -54,8 +86,6 @@ class ThemeProvider with ChangeNotifier {
     
   );
 
-
-
   ThemeMode _themeMode = ThemeMode.dark;
 
   // GETTERS
@@ -72,11 +102,5 @@ class ThemeProvider with ChangeNotifier {
     }
     notifyListeners();
   }
-
-  
-
-
-
-
 
 }
