05f4f22acbfc9ce1bad5819ed87a4cbb084b68f5
Adem ERCAN <ademerc.ss@gmail.com>
2025-06-19 09:56:20 +0300

    19.06.2025 - 09:56

--- lib/views/post_screen/post_screen.dart
+++ lib/views/post_screen/post_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:sosyal_ag/init.dart';
 import 'package:sosyal_ag/models/post_model.dart';
 import 'package:sosyal_ag/models/user_model.dart';
+import 'package:sosyal_ag/utils/locator.dart';
 import 'package:sosyal_ag/view_models/post_view_model.dart';
 import 'package:sosyal_ag/views/post_screen/comment_area/comment_area_widget.dart';
 import 'package:sosyal_ag/views/post_screen/post_area/post_area_widget.dart';
@@ -14,8 +16,9 @@ class PostScreen extends StatelessWidget {
   final UserModel author;  */
 
   final Map<String, dynamic> mapData;
+  final Init _init = locator<Init>();
 
-  const PostScreen({
+   PostScreen({
     super.key,
     required this.mapData,
     /* required this.post,
@@ -55,22 +58,63 @@ class PostScreen extends StatelessWidget {
               }
             },
             itemBuilder:
-                (BuildContext context) => [
-                  PopupMenuItem<String>(
-                    value: 'delete',
-                    child: Row(
-                      children: [
-                        Icon(Icons.delete, color: theme.colorScheme.error),
-                        const SizedBox(width: 8),
-                        Text(
-                          'Sil',
-                          style: TextStyle(color: theme.colorScheme.error),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                (BuildContext context) => _init.user?.uid == author.uid
+                                            ? <PopupMenuItem<String>>[
+                                              PopupMenuItem<String>(
+                                                value: 'delete',
+                                                child: Row(
+                                                  children: [
+                                                    Icon(
+                                                      Icons.delete,
+                                                      color:
+                                                          theme
+                                                              .colorScheme
+                                                              .error,
+                                                      size: 20,
+                                                    ),
+                                                    const SizedBox(width: 8),
+                                                    Text(
+                                                      'Sil',
+                                                      style: TextStyle(
+                                                        color:
+                                                            theme
+                                                                .colorScheme
+                                                                .error,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ]
+                                            : <PopupMenuItem<String>>[
+                                              PopupMenuItem<String>(
+                                                value: 'block',
+                                                child: Row(
+                                                  children: [
+                                                    Icon(
+                                                      Icons.block,
+                                                      color:
+                                                          theme
+                                                              .colorScheme
+                                                              .error,
+                                                      size: 20,
+                                                    ),
+                                                    const SizedBox(width: 8),
+                                                    Text(
+                                                      'Engelle',
+                                                      style: TextStyle(
+                                                        color:
+                                                            theme
+                                                                .colorScheme
+                                                                .error,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ]
           ),
+    
         ],
       ),
       body: SingleChildScrollView(
